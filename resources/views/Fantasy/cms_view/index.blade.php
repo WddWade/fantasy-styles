738cf3b341d6cbd8953766491cec2449dc2bf82f
xwadex <5094484+WddWade@users.noreply.github.com>
2025-09-03 17:05:40 +0800

    Wade Update

--- resources/views/Fantasy/cms_view/index.blade.php
+++ resources/views/Fantasy/cms_view/index.blade.php
@@ -58,36 +58,47 @@
                         <div class="scroll-wrapper content-scrollbox" style="position: relative;">
                             <div class="content-scrollbox scroll-content" style="height: 877px; margin-bottom: 0px; margin-right: 0px; max-height: none;">
                                 <div class="content-wrap main-table index-table-div" data-tableid="new_cms_table">
+
+                                    {{-- wade:add --}}
+                                    <div class="cms_index">
+                                        <div class="main-title">CMS</div>
+                                    </div>
+
+                                    {{-- wade:add --}}
                                     <div class="content-head cms-index_table" data-can_review="1" data-edit="1" data-delete="1" data-create="1" data-model="Class_faq" data-page="1" data-pn="1" data-auth="206" data-pagetitle="常見問題分類" data-issearch="1" data-isbatch="1" data-isclone="1" data-isexport="">
                                         <h1>品牌總覽</h1>
                                     </div>
                                     <div class="content-body">
                                         <div class="branch_wrapper">
-                                                @foreach($branchMenuList['list'] as $key => $row)
-                                            <div class="branch_area">
-                                                <div class="branch_header">
-                                                    <div class="branch_title">{{ $row['title'] }}</div>
-                                                    <div class="branch_title">{{ $row['en_title'] }}</div>
-                                                    @if($row['blade_template'] == 1)
-                                                    <div class="branch_title"><a target="_blank" href="{{preg_replace('/^(https?:\/\/)([^.]+)(\..+)$/', '$1' . $row['url_title'] . '$3', url(''))}}">{{preg_replace('/^(https?:\/\/)([^.]+)(\..+)$/', '$1' . $row['url_title'] . '$3', url(''))}}</a></div>
-                                                    @else
-                                                    <div class="branch_title"><a target="_blank" href="{{url($row['url_title'])}}">{{url($row['url_title'])}}</a></div>
-                                                    @endif
-                                                        </div>
-                                                <div class="branch_lang">
+                                            @foreach($branchMenuList['list'] as $key => $row)
+                                                <div class="branch_area">
+                                                    <div class="branch_header">
+                                                        <div class="branch_title">{{ $row['title'] }}</div>
+                                                        <div class="branch_title">{{ $row['en_title'] }}</div>
+                                                        @if($row['blade_template'] == 1)
+                                                            <div class="branch_title">
+                                                                <a target="_blank" href="{{preg_replace('/^(https?:\/\/)([^.]+)(\..+)$/', '$1' . $row['url_title'] . '$3', url(''))}}">{{preg_replace('/^(https?:\/\/)([^.]+)(\..+)$/', '$1' . $row['url_title'] . '$3', url(''))}}</a>
+                                                            </div>
+                                                        @else
+                                                            <div class="branch_title">
+                                                                <a target="_blank" href="{{url($row['url_title'])}}">{{url($row['url_title'])}}</a>
+                                                            </div>
+                                                        @endif
+                                                        <div class="branch_lang">
                                                             @foreach($row['list'] as $key2 => $row2)
-                                                    <div><a href="{{ $row2['link'] }}">{{ $row2['title'] }}</a></div>
+                                                                <div>
+                                                                    <a href="{{ $row2['link'] }}">{{ $row2['title'] }}</a>
+                                                                </div>
                                                             @endforeach
                                                         </div>
-                                            </div>
-                                                @endforeach
+                                                    </div>
+                                                </div>
+                                            @endforeach
                                         </div>
-
                                     </div>
                                 </div>
                             </div>
                         </div>
-
                     </div>
                     <!-- 右邊 PAGE CONTENT -->
                 </div>
